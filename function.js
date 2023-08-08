26276cc38f1a23241fe766a0f4737766c9e748ab
Nayde227 <naylimar27@gmail.com>
2023-08-08 16:33:00 -0400

    add getFileExtension function

--- function.js
+++ function.js
@@ -1,5 +1,6 @@
 const fs = require('fs');
 
+//Función para leer el archivo
 fs.readFile('./README.md',  function(err, data){
 if(err){
     console.log('ocurrió un error');
@@ -7,3 +8,11 @@ if(err){
     console.log(data.toString());
 }
 });
+
+//Función para obtener la extensión del archivo
+let file = 'README.md';
+
+
+function getFileExtension(file){
+    return file.split('.').pop();
+} console.log(getFileExtension(file));
