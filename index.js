e6e1ae6b5506aeedfa533fdd49181d6ee4d0db1a
Nayde227 <naylimar27@gmail.com>
2023-08-22 16:58:07 -0400

    fisrt test passed

--- index.js
+++ index.js
@@ -2,16 +2,21 @@ const { existsPath,readFile, getFileExtension} = require('./function');
 
 const mdLinks = (path, options) => {
 return new Promise((resolve, reject) => {
+    if(existsPath(path)) {
+        console.log(existsPath(path))
+    } else {
+        reject('La ruta no existe')
+    }
+ 
+    console.log(getFileExtension(path))
     
- console.log(existsPath(path))
- console.log(getFileExtension(path))
- console.log(readFile(path))
+    console.log(readFile(path))
  
 })
 
 
 }
-mdLinks('README.md')
+mdLinks('READM.d')
 .then((result) => {
     console.log(result)
 })
--- test/md-links.spec.js
+++ test/md-links.spec.js
@@ -1,19 +1,21 @@
+const { existsPath } = require('../function.js');
 const { mdLinks }  = require('../index.js');
 
 
-describe('rechazar si el path no existe', () => {
+describe('mdLinks', () => {
 
   it('should...', () => {
     console.log('FIX ME!');
   });
 
-  /*it('debería devolver una promesa', () => {
-    expect(mdLinks()).toBe(typeof Promise);
-  });*/
+  /*
+  it('should return a promise', () => {
+   expect(mdLinks()).toBe(typeof Promise)
+  })*/
 
-  it('debería rechazar la promesa si el path no existe', () => {
-    return mdLinks('./EstaRutaNo.md').catch(() => {
-      expect(error).toBe('file not exists')
+  it('debe rechazar cuando el path no existe', () => {
+    return mdLinks('./EstaRutaNo.md').catch((error) => {
+      expect(error).toBe('La ruta no existe')
     })
   })
   
